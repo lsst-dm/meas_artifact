94a24ff080da36b91789ad5f6ed5d92025ce60a3
Steven Bickerton <steven.bickerton@gmail.com>
2015-08-05 09:47:48 +0900

    woops ... forgot __init__.py

---
+++ python/lsst/meas/satellite/__init__.py
@@ -0,0 +1,7 @@
+
+from satelliteTrail   import *
+from satelliteUtils   import *
+from hough            import *
+from momentCalculator import *
+from satelliteFinder  import *
+from satelliteTask    import *
